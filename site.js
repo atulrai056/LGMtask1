ead240cf257d7afa68611bfcea97dfcea489a8b1
ATUL RAI <84737637+atulrai056@users.noreply.github.com>
2021-08-16 20:27:05 +0530

    Add files via upload

---
+++ site.js
@@ -0,0 +1,18 @@
+console.log("ATUL")
+function videourl(a)
+{
+    document.getElementById("slider1").src = a;
+}
+function imgurl(a)
+{
+    document.getElementById("wimg").src=a;
+}
+var counter = 1;
+setInterval(function()
+{
+    document.getElementById('radio'+ counter).checked=true;
+    counter++;
+    if(counter > 4){
+        counter = 1;
+    }   
+},5000);
